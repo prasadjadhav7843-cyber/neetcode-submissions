7fdbc4b223b74ba108ea92f4459103154311788f
prasadjadhav7843-cyber <prasadjadhav7843@gmail.com>
2026-09-17 11:02:59 +0530

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer>set=new HashSet<>();
+
+        for(int i=0;i<nums.length;i++){
+            if(set.contains(nums[i])){
+                return true;
+            }
+            set.add(nums[i]);
+        }
+        return false;
+        //   Arrays.sort(nums);
+        // for(int i=1;i<nums.length;i++){
+        //     if(nums[i-1]==nums[i]){
+        //         return true;
+        //     }
+        // }
+        // return false;
+    }
+}
